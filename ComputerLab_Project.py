09aa570755715f446ed627da5aeb77d757145d14
Xiaolin (Stuart) Cheng <a565837019@qq.com>
2019-04-13 05:15:01 +0800

    Add files via upload

--- ComputerLab_Project.py
+++ ComputerLab_Project.py
@@ -9,7 +9,7 @@
 #random number generator library and NumPy
 import random
 import numpy as np
-import matplotlib as mplot
+import matplotlib.pyplot as plt
 
 #Returns x(n+1)
 def expanding_seq(x,a):
@@ -25,7 +25,7 @@ def convergent_point(a,x_init,n):
     #for loop repeat n times
     for i in range(n):
         x = expanding_seq(x,a)
-    return a,x
+    return [a,x]
 
 temp = [convergent_point(1.,random.random(),100)]
 print (temp)
@@ -33,26 +33,35 @@ print (temp)
 #An array that saves all recorded values of (a) and (converging x)
 an_array = []
 an_array.append(temp)
-
+del temp
 
 #Question 3 
  
 
-#Function intakes a, sample_size and n
+#Function intakes a, x_init_size and n
 #returns list consist of elements (a,x_), where x* is convergence point
-def convergent_point_list(a,sample_size,n): #!!!*n might not be taken!!!
+def convergent_point_list(a,x_init_size,n): 
     #List of x convergence points
     x_conv_list = []
-    #np.random.random_sample(sample_size).tolist() function
-    #returns a list of random samples of initial condition between 0 and 1
-    #with sample size of sample_size
-    for x_init in np.random.random_sample(sample_size).tolist():
-        x_conv_list.append([convergent_point(a,x_init,n)])
+    for x_init in np.random.random_sample(x_init_size).tolist():#repeat with (sample_size) number of non_repeated random value
+        x_conv_list.append(convergent_point(a,x_init,n))
     return x_conv_list
+
+a_NumPy_array = np.array(convergent_point_list(1,50,100))
 
 #Question 4
 
+temp = []
+#the line gives a random sample size of 2000 in the interval [1.5,4] in numerical order
+for a in sorted((np.random.sample(2000)*2.5+1.5).tolist()):
+    #a temporary variable is used to collect the data list
+    temp += convergent_point_list(a,50,100)
+another_NumPy_array = np.array(temp)
+del temp
+#print (another_NumPy_array[:50])
 
+#Question 5:
 
-
+plt.scatter(another_NumPy_array[:,0],another_NumPy_array[:,1])
+plt.show()
             
